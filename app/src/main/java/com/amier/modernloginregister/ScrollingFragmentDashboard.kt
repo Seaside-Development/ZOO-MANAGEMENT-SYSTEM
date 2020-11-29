ecfae7512a94d5abbafd29f70a463d8c884c0729
Theo Taylor <theotaylor@taylord.tech>
2020-11-29 09:24:25 -0400

    Set up fragment manager insode fragment instead of activity

--- app/src/main/java/com/amier/modernloginregister/ScrollingFragmentDashboard.kt
+++ app/src/main/java/com/amier/modernloginregister/ScrollingFragmentDashboard.kt
@@ -1,17 +1,35 @@
 package com.amier.modernloginregister
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 class ScrollingFragmentDashboard : Fragment() {
 
+    // Declare var to store parent activity
+    lateinit var mActivity: AppCompatActivity;
+
     override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, state: Bundle?): View? {
         val viewRoot = inflater.inflate(R.layout.fragment_scrolling_dashboard, root, false);
-
+        val btnChangeFrag = viewRoot.findViewById<Button>(R.id.change_frag)
+        val ticketMasterFrag = TicketMasterFragment()
+        btnChangeFrag.setOnClickListener {
+            // Use parent activity to perform fragment transaction
+            mActivity.supportFragmentManager.beginTransaction()
+                .replace(R.id.root_container, ticketMasterFrag)
+                .commitAllowingStateLoss()
+        }
         return viewRoot
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        // Get parent activity from context
+        mActivity = context as AppCompatActivity;
     }
 }
