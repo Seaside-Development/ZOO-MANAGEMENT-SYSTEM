bb2f13fcc10711ab6322b4ba3e5556024031aac5
RicePeas <63255164+RicePeas@users.noreply.github.com>
2021-01-05 02:46:37 -0400

    Update MainActivity.kt

--- app/src/main/java/com/swen3/zooman/ui/MainActivity.kt
+++ app/src/main/java/com/swen3/zooman/ui/MainActivity.kt
@@ -12,8 +12,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.swen3.zooman.R
-import com.swen3.zooman.ZooLab
-import com.swen3.zooman.database.ZooMngHelper
+
 
 class MainActivity : AppCompatActivity() {
 
