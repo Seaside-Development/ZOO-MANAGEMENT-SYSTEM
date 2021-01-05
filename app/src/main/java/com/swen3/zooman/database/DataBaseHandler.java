26af80e275881469b108300f0ac68705428ad278
natebrate <nbrathwaite16@gmail.com>
2021-01-05 08:25:43 -0400

    Animal list

--- app/src/main/java/com/swen3/zooman/database/DataBaseHandler.java
+++ app/src/main/java/com/swen3/zooman/database/DataBaseHandler.java
@@ -1,19 +1,12 @@
 package com.swen3.zooman.database;
 
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
-
-import com.swen3.zooman.Customer;
-import com.swen3.zooman.Species;
 
 
-public class DataBaseHelper extends SQLiteOpenHelper {
+class DataBaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "users";
     private static final String DB_TABLE = "userTable";
@@ -23,8 +16,13 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     private static String USERNAME= "UserName";
     private static String PASS= "Password";
 
-    public DataBaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    //Query
+    private static final String CREATE_TABLE = "CREATE TABLE"+ DB_TABLE+" ("+
+            ID
+            ""
+
+    public DataBaseHelper( Context context) {
+        super(context, DB_NAME, null, 1);
     }
 
     @Override
