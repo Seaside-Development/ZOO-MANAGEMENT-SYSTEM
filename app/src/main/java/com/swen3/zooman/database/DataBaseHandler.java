ad976ba19b16662ad0c5dec37195bba520e021da
natebrate <nbrathwaite16@gmail.com>
2021-01-05 08:43:50 -0400

    DataBaseHelper

--- app/src/main/java/com/swen3/zooman/database/DataBaseHandler.java
+++ app/src/main/java/com/swen3/zooman/database/DataBaseHandler.java
@@ -1,25 +1,26 @@
 package com.swen3.zooman.database;
 
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 
-class DataBaseHelper extends SQLiteOpenHelper {
+public class DataBaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "users";
     private static final String DB_TABLE = "userTable";
 
     //Columns
     private static String EMAIL = "email";
-    private static String USERNAME= "UserName";
-    private static String PASS= "Password";
+    private static String USERNAME= "username";
+    private static String PASS= "password";
 
     //Query
-    private static final String CREATE_TABLE = "CREATE TABLE"+ DB_TABLE+" ("+
-            ID
-            ""
+    private static final String CREATE_TABLE = "CREATE TABLE "+ DB_TABLE+" ("+
+            EMAIL + "VARCHAR PRIMARY KEY AUTOINCREMENT," +
+    USERNAME + "TEXT " + PASS + "TEXT" + ")";
 
     public DataBaseHelper( Context context) {
         super(context, DB_NAME, null, 1);
@@ -27,11 +28,24 @@ class DataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE);
 
+        onCreate(db);
     }
+
+    //CREATE METHOD TO INSERT DATA
+    public boolean insertData(String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(EMAIL, email);
+
+        long result = db.insert(DB_TABLE, null, contentValues);
+        return result != -1; //if result =-1 doesn't insert
+    }
+
 }
--- app/src/main/java/com/swen3/zooman/ui/animals/AnimalItemsFragment.java
+++ app/src/main/java/com/swen3/zooman/ui/animals/AnimalItemsFragment.java
@@ -1,60 +1,15 @@
-package com.swen3.zooman.ui.animals
+package com.swen3.zooman.ui.animals;
 
-import android.content.Context
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.swen3.zooman.R
-import com.swen3.zooman.database.DataBaseHandler
-import java.util.*
+import androidx.fragment.app.Fragment;
+
+import com.swen3.zooman.database.DataBaseHelper;
 
 
 /**
  * A fragment representing a list of Items.
  */
-class AnimalItemsFragment : Fragment() {
-
-    private var columnCount = 1
-    lateinit var mActivity: AppCompatActivity;
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = 1
-        }
-    }
+class AnimalItemsFragment extends Fragment{
+    DataBaseHelper db;
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_animal_item_list, container, false)
-
-        //val animals = mActivity.resources.getStringArray(R.array.animals);
-        val myArrayList: List<String> = Arrays.asList(*resources.getStringArray(R.array.animals))
-
-        /* // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = LinearLayoutManager(context)
-                adapter = AnimalItemRecyclerViewAdapter(myArrayList)
-            }
-        }
-        return view
-    }*/
-
-        /*fun onAttach(context: Context) {
-        super.onAttach(context)
-        mActivity = context as AppCompatActivity;
-        }
-    }*/
-    }
 }
