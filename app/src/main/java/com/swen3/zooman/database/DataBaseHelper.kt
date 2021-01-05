53b306b16744eb5f222363e6457b6b531d2ee952
natebrate <nbrathwaite16@gmail.com>
2021-01-05 09:50:40 -0400

    DataBaseHelper

--- app/src/main/java/com/swen3/zooman/database/DataBaseHelper.kt
+++ app/src/main/java/com/swen3/zooman/database/DataBaseHelper.kt
@@ -1,59 +1,101 @@
-package com.swen3.zooman.database;
+package com.swen3.zooman.database
 
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+import com.swen3.zooman.Customer
+import com.swen3.zooman.Species
 
-public class DataBaseHelper extends SQLiteOpenHelper {
 
-    private static final String DB_NAME = "users";
-    private static final String DB_TABLE = "userTable";
+val DATABASE_NAME="ZooMngMnt"
 
-    //Columns
-    private static final String EMAIL = "EMAIL";
-    private static final String USERNAME= "USERNAME";
-    private static final String PASS= "PASSWORD";
+val TABLE_NAME="Customers"
+val COL_EMAIL="email"
+val COL_USERNAME="UserName"
+val COL_PASS="Password"
 
-    //Query
-    private static final String CREATE_TABLE = "CREATE TABLE "+ DB_TABLE+" ("+
-            EMAIL + "VARCHAR PRIMARY KEY AUTOINCREMENT, "+
-            USERNAME+ " TEXT" + PASS+ "TEXT "+ ")";
 
-    public DataBaseHelper(Context context) {
-        super(context, DB_NAME, null, 1);
+
+const val NAME = "species"
+object Cols {
+    val COL_ID="SpeciesID"
+    val COL_SPEC="Species_Name"
+    val COL_CLASS="Animal_CLass"
+    val COL_FOOD="Diet"
+    val COL_DESC="Description"
+}
+
+
+
+class DataBaseHandler (var context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null,1){
+
+
+
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createSpeciesTable="CREATE TABLE "+ NAME + " ("+ Cols.COL_ID +  " INT PRIMARY KEY, " +
+                Cols.COL_SPEC + " VARCHAR(50), " + Cols.COL_CLASS +" VARCHAR(50), " + Cols.COL_FOOD + " VARCHAR(50), " + Cols.COL_DESC + " VARCHAR(100) )"
+
+        db?.execSQL(createSpeciesTable)
+
+
+        val createCusTable="CREATE TABLE " + TABLE_NAME + " (" + COL_EMAIL+ " VARCHAR(50) PRIMARY KEY, " +
+                COL_USERNAME + " VARCHAR(50), " +
+                COL_PASS + " VARCHAR(50) )"
+
+        db?.execSQL(createCusTable)
+
+
+
+
+
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE);
+    override fun onUpgrade (db :SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+
+
+
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+ DB_TABLE);
+    fun insertData(customer: Customer) {
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(COL_EMAIL, customer.email)
+        cv.put(COL_USERNAME, customer.userName)
+        cv.put(COL_PASS,customer.password)
 
-        onCreate(db);
+        val result = db.insert(TABLE_NAME,null,cv)
+        if (result== (-1).toLong())
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+        else
+            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
     }
 
-    //CREATE METHOD TO INSERT DATA
-    public boolean insertData(String email) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(EMAIL, email);
 
-        long result = db.insert(DB_TABLE, null, contentValues);
-        return result != -1; //if result =-1 doesn't insert
+    fun readData(): MutableList<Species>
+    {
+        var list : MutableList<Species> = ArrayList()
+
+        val db=this.readableDatabase
+        val query="Select * from "+ NAME
+        val result=db.rawQuery(query, null)
+        if (result.moveToFirst()){
+            do {
+                var species = Species()
+                species.specID= result.getString(result.getColumnIndex(Cols.COL_ID)).toInt();
+                species.specName=result.getString(result.getColumnIndex(Cols.COL_SPEC))
+                species.aniClass=result.getString(result.getColumnIndex(Cols.COL_CLASS))
+                species.diet=result.getString(result.getColumnIndex(Cols.COL_FOOD))
+                species.description=result.getString(result.getColumnIndex(Cols.COL_DESC))
+                list.add(species)
+            } while (result.moveToNext())
+        }
+        result.close()
+        db.close()
+        return list
     }
 
-    //Create method to view data
-
-    public Cursor viewData(){
-        SQLiteDatabase db = this.getReadableDatabase();
-        String query = "Select * from "+ DB_TABLE;
-        Cursor cursor = db.rawQuery(query, null);
-        return cursor;
-    }
 }
