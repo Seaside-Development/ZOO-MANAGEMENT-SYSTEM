a031af0f341bf24d392c6591902da0d2065f559a
natebrate <nbrathwaite16@gmail.com>
2021-01-05 09:00:06 -0400

    DataBaseHelper

--- app/src/main/java/com/swen3/zooman/database/DataBaseHelper.java
+++ app/src/main/java/com/swen3/zooman/database/DataBaseHelper.java
@@ -3,6 +3,7 @@ package com.swen3.zooman.database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -13,16 +14,16 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     private static final String DB_TABLE = "userTable";
 
     //Columns
-    private static String EMAIL = "email";
-    private static String USERNAME= "username";
-    private static String PASS= "password";
+    private static String EMAIL = "EMAIL";
+    private static String USERNAME= "USERNAME";
+    private static String PASS= "PASSWORD";
 
     //Query
-    private static final String CREATE_TABLE = "CREATE TABLE "+ DB_TABLE+" ("+
-            EMAIL + "VARCHAR PRIMARY KEY AUTOINCREMENT," +
-    USERNAME + "TEXT " + PASS + "TEXT" + ")";
+    private static final String CREATE_TABLE = "CREATE TABLE "+ DB_TABLE+ " ("+
+            EMAIL + " VARCHAR PRIMARY KEY AUTOINCREMENT, " +
+            USERNAME + "TEXT " + PASS + "TEXT " + ")";
 
-    public DataBaseHelper( Context context) {
+    public DataBaseHelper(Context context) {
         super(context, DB_NAME, null, 1);
     }
 
@@ -33,7 +34,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS "+ DB_TABLE);
 
         onCreate(db);
     }
@@ -48,4 +49,13 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         return result != -1; //if result =-1 doesn't insert
     }
 
+    //Create method to view data
+
+    public Cursor viewData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "Select * from "+ DB_TABLE;
+
+        Cursor cursor = db.rawQuery(query, null);
+        return cursor;
+    }
 }
--- app/src/main/java/com/swen3/zooman/ui/animals/AnimalItemsFragment.java
+++ app/src/main/java/com/swen3/zooman/ui/animals/AnimalItemsFragment.java
@@ -1,7 +1,10 @@
 package com.swen3.zooman.ui.animals;
 
+import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 
+import com.swen3.zooman.R;
 import com.swen3.zooman.database.DataBaseHelper;
 
 
@@ -10,6 +13,18 @@ import com.swen3.zooman.database.DataBaseHelper;
  */
 class AnimalItemsFragment extends Fragment{
     DataBaseHelper db;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_animal_item);
+
+        db = new DataBaseHelper(this);
+
+    }
+
+    private void setContentView(int fragment_animal_item) {
+    }
 
 
 }
